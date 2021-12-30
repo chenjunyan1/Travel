a483df271305a0288dd043a6ac10cfd8ad100a05
情 之 雁 伤 <10019800+junyan101@user.noreply.gitee.com>
2021-12-30 11:43:56 +0800

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import './App.css'
+import { StrictMode, lazy, Suspense, Component } from 'react';
+
+import './App.css';
 import ReactDOM from 'react-dom';
-import Data from './data.json'
+import Data from './data.json';
 import Avatar from '@material-ui/core/Avatar';
 
-import { useScrollTrigger, Zoom,Typography,Box, ButtonGroup,Button} from '@mui/material';
+import { useScrollTrigger, Zoom, Typography, Box, ButtonGroup, Button } from '@mui/material';
+
 function num_img(data, num) {
   let arr = [];
   for (let m = 1; m <= num; m++) {
@@ -13,6 +15,8 @@ function num_img(data, num) {
   return arr;
 }
 let scrollTop;
+
+
 export default class App extends Component {
   constructor(props) {
     super(props)
@@ -88,100 +92,104 @@ export default class App extends Component {
   //   );
   // }
 
-    render() {
-      const { num } = this.state;
-      return (
-        <div>
-          {/* 页面1 */}
-          <div className="page_1">
-            <div>
-              <div className="title">
-                ShenZhen_Travel
-              </div>
-              <div className="we">
-                <Avatar
-                  alt="陈"
-                  src={"Character/we/chenjunyan.jpg"}
-                  sx={{ width: 110, height: 110 }}
-                />
-                <Avatar
-                  alt="李"
-                  src={"Character/we/lixiaojuan.png"}
-                  sx={{ width: 110, height: 110 }}
-                />
-              </div>
+  render() {
+    const { num } = this.state;
+    return (
+      <div>
+        {/* 页面1 */}
+        <div className="page_1">
+          <div>
+            <div className="title">
+              ShenZhen_Travel
+            </div>
+            <div className="we">
+              <Avatar
+                alt="陈"
+                src={"Character/we/chenjunyan.jpg"}
+                sx={{ width: 110, height: 110 }}
+              />
+              <Avatar
+                alt="李"
+                src={"Character/we/lixiaojuan.png"}
+                sx={{ width: 110, height: 110 }}
+              />
+            </div>
 
-              <div>
-                <Typography
-                  component="div"
-                  variant="body1"
-                  style={{
-                    height: 100,
-                    width: '100%',
-                    position: 'relative',
+            <div>
+              <Typography
+                component="div"
+                variant="body1"
+                style={{
+                  height: 100,
+                  width: '100%',
+                  position: 'relative',
+                }}
+              >
+                <Box
+                  sx={{
+                    bgcolor: '#177cb0',
+                    color: 'white',
+                    p: 2,
+                    position: 'absolute',
+                    top: 40,
+                    left: '40%',
+                    zIndex: 'tooltip',
                   }}
                 >
-                  <Box
-                    sx={{
-                      bgcolor: '#177cb0',
-                      color: 'white',
-                      p: 2,
-                      position: 'absolute',
-                      top: 40,
-                      left: '40%',
-                      zIndex: 'tooltip',
-                    }}
-                  >
-                    LiXiaoJuan
-                  </Box>
-                  <Box
-                    sx={{
-                      bgcolor: 'cadetblue',
-                      color: 'text.primary',
-                      p: 2,
-                      position: 'absolute',
-                      top: 0,
-                      left: '43%',
-                      zIndex: 'modal',
-                    }}
-                  >
-                    ChenJunYan
-                  </Box>
-                </Typography>
-              </div>
+                  LiXiaoJuan
+                </Box>
+                <Box
+                  sx={{
+                    bgcolor: 'cadetblue',
+                    color: 'text.primary',
+                    p: 2,
+                    position: 'absolute',
+                    top: 0,
+                    left: '43%',
+                    zIndex: 'modal',
+                  }}
+                >
+                  ChenJunYan
+                </Box>
+              </Typography>
             </div>
           </div>
-          {/* button 日期 */}
-          <div className="page_2">
-            <div className="riqi">
-              <ButtonGroup disableElevation
-                variant="contained"
-                fullWidth={true}
-              >
-                <Button
-                  onClick={this.handclick.bind(this, 1)}
-                >2021.10.22</Button>
-                <Button
-                  onClick={this.handclick.bind(this, 2)}
-                >2021.10.23</Button>
-              </ButtonGroup>
-              <ButtonGroup disableElevation
-                variant="contained"
-                fullWidth={true}
-                color="warning"
-              >
-                <Button
-                  onClick={this.handclick.bind(this, 3)}
-                >2021.10.24</Button>
-                <Button
-                  onClick={this.handclick.bind(this, 4)}
-                >2021.10.25</Button>
-              </ButtonGroup>
-            </div>
+        </div>
+        {/* button 日期 */}
+        <div className="page_2">
+          <div className="riqi">
+            <ButtonGroup disableElevation
+              variant="contained"
+              fullWidth={true}
+            >
+              <Button
+                onClick={this.handclick.bind(this, 1)}
+              >2021.10.22</Button>
+              <Button
+                onClick={this.handclick.bind(this, 2)}
+              >2021.10.23</Button>
+            </ButtonGroup>
+            <ButtonGroup disableElevation
+              variant="contained"
+              fullWidth={true}
+              color="warning"
+            >
+              <Button
+                onClick={this.handclick.bind(this, 3)}
+              >2021.10.24</Button>
+              <Button
+                onClick={this.handclick.bind(this, 4)}
+              >2021.10.25</Button>
+            </ButtonGroup>
           </div>
-          {/* 10_22 */}
-          <div className="img_all">
-            {(num == 1) ? (
+        </div>
+        {/* 10_22 */}
+        <div className="img_all">
+          {(num == 1) ? (
+            <Suspense fallback={
+              <div className='load'>
+                <div>11111111111111111111111111111111111111111111111</div>
+              </div>}>
               <div>
                 {num_img("10_22", 6).map((item, index) => {
                   console.log(num == 1)
@@ -193,65 +201,67 @@ export default class App extends Component {
                   )
                 })
                 }
-              </div>) : null}
-            {/* 10_23 */}
-            {(num == 2) ? (
+              </div>
+            </Suspense>
+          ) : null}
+          {/* 10_23 */}
+          {(num == 2) ? (
+            <div>
+              {num_img("10_23", 21).map((item, index) => {
+                console.log(num == 2)
+                return (
+                  <div key={index}>
+                    {(Data[23][index] != "") ? (<div><div>{Data[23][index]}</div></div>) : null}
+                    <img src={item} alt="照片正在路上" />
+                  </div>
+                )
+              })
+              }
+            </div>) : null}
+          {/* 10_24 */}
+          {(num == 3) ? (
+            <div>
               <div>
-                {num_img("10_23", 21).map((item, index) => {
-                  console.log(num == 2)
-                  return (
-                    <div key={index}>
-                      {(Data[23][index] != "") ? (<div><div>{Data[23][index]}</div></div>) : null}
-                      <img src={item} alt="照片正在路上" />
-                    </div>
-                  )
-                })
-                }
-              </div>) : null}
-            {/* 10_24 */}
-            {(num == 3) ? (
-              <div>
-                <div>
-                  <img src="ShenZhen/Chen_junyan.jpg" alt="照片正在路上" />
-                </div>
-                {num_img("10_24", 61).map((item, index) => {
-                  console.log(num == 3)
-                  return (
-                    <div key={index}>
-                      <img src={item} alt="照片正在路上" />
-                      <div><div>俊雁同学参加2021界学生开源年会</div></div>
-                    </div>
-                  )
-                })
-                }
-              </div>) : null}
-            {/* 10_25 */}
-            {(num == 4) ? (
-              <div>
-                {num_img("10_25", 77).map((item, index) => {
-                  console.log(num == 4)
-                  return (
-                    <div key={index}>
-                      <img src={item} alt="照片正在路上" />
-                      <div><div>在深圳的最后一天[海边]</div></div>
-                    </div>
-                  )
-                })
-                }
-              </div>) : null}
-          </div>
-          {/* 作者声明 */}
-          <div className="page_end">
-            Wonderland © Website 2021
-          </div>
-          {
-            (scrollTop > 500) ? (
-              <div className="zhiding">
-                <img onClick={this.topping.bind(this)} src="Character/置顶.svg" />
-              </div>) : null
-          }
-        </div >
+                <img src="ShenZhen/Chen_junyan.jpg" alt="照片正在路上" />
+              </div>
+              {num_img("10_24", 61).map((item, index) => {
+                console.log(num == 3)
+                return (
+                  <div key={index}>
+                    <img src={item} alt="照片正在路上" />
+                    <div><div>俊雁同学参加2021界学生开源年会</div></div>
+                  </div>
+                )
+              })
+              }
+            </div>) : null}
+          {/* 10_25 */}
+          {(num == 4) ? (
+            <div>
+              {num_img("10_25", 77).map((item, index) => {
+                console.log(num == 4)
+                return (
+                  <div key={index}>
+                    <img src={item} alt="照片正在路上" />
+                    <div><div>在深圳的最后一天[海边]</div></div>
+                  </div>
+                )
+              })
+              }
+            </div>) : null}
+        </div>
+        {/* 作者声明 */}
+        <div className="page_end">
+          Wonderland © Website 2021
+        </div>
+        {
+          (scrollTop > 500) ? (
+            <div className="zhiding">
+              <img onClick={this.topping.bind(this)} src="Character/置顶.svg" />
+            </div>) : null
+        }
+      </div >
 
-      )
-    }
+    )
   }
+}
